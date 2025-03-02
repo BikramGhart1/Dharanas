1c1267944ca28eeac62a411ce781a0f952c55fc0
BikramGhart1 <coludingrain@gmail.com>
2025-03-02 19:11:40 +0545

    get request for the user details

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -1,9 +1,34 @@
-import React from 'react'
+import axios from 'axios';
+import React, { useEffect, useState } from 'react'
 
 export default function HomePage() {
+  const [userDetails, setUserDetails] = useState(null);
+  const getUserDetails = async () => {
+    try {
+      const token = localStorage.getItem('token');
+      const user = await axios.get('http://localhost:3000/user/userDetails', {
+        headers: {
+          'Authorization': `Bearer ${token}`
+        }
+      }
+      )
+      const userData=user.data.user;
+      console.log(user);
+      setUserDetails(userData);
+    } catch (err) {
+      console.error("Error while fetching user data: ", err);
+    }
+  }
+  useEffect(() => {
+    getUserDetails();
+  }, [])
   return (
-    <div>
-      <p className='bg-red-500'>Yo yo yo This is home page</p>
+    <div className='text-text bg-background min-h-svh flex justify-center items-center'>
+      {userDetails?(
+        <p>Welcome {userDetails.username}</p>
+      ):(
+        <p>Loading....</p>
+      )} 
     </div>
   )
 }
